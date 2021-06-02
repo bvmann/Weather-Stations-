4c02bfe961b3cf45104a264ba3ea79bd34f3157d
britt mann <brittvmann@gmail.com git config --global user.name bvmann>
2021-06-01 20:50:45 -0700

    app.py

--- app.py
+++ app.py
@@ -5,8 +5,24 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 app = Flask(__name__)
+
+Base = automap_base()
+# reflect an existing database into a new model
+Base.prepare(engine, reflect = True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+df = session.query(Measurement.date, Measurement.prcp)
+
+date_temp = ({Measurement.date[0]:Measurement.date[1] for  Measurement.date in df})
 
 @app.route("/")
 def welcome():
@@ -21,7 +37,39 @@ def welcome():
     )
 @app.route("/api/v1.0/precipitation")
 def rainy(): 
-    return()
+    
+    
+    
+    return jsonify(date_temp)
+session.close()
+
+@app.route("/api/v1.0/stations")
+def station():
+    session = Session(engine)
+    stations = session.query(Station.station)
+    stat = []
+    for x in stations:
+        stat.append(x)
+
+    return jsonify(stat)
+session.close()
+@app.route("/api/v1.0/tobs")
+def tobs():
+    
+    session = Session(engine)
+    station_7_=session.query(Measurement.date,Measurement.tobs,Measurement.station).filter(Measurement.station=='USC00519281')
+    station7=[]
+    for x in station_7_: 
+       station7.append(x)
+    
+    
+    return jsonify(station7)
+
+@app.route("/api/v1.0/startdate/<start>")
+def start(): 
+    session=Session(engine)
+    return f"enter your start date "
+
 
 if __name__ == '__main__':
     app.run(debug=True)
